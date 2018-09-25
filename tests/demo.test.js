6dd3bb584f6a63af500de6ae2883e717426a3831
kerloch <skerloch@free.fr>
2018-09-25 12:24:45 +0200

    Update compare ton name

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -5,6 +5,6 @@ this.name = name;
 describe("Checking if the user is created correctly", function(){
 it("should create the user with the correct name", function(){
 var tom = new User("tom");
-tom.name.should.be.equal("tom");
+tom.name.should.be.equal("tommy");
 });
 });
